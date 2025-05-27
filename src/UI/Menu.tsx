16d3bf95897d903d326e1422232f289514864ebc
DoctorTone <tony@drt-software.com>
2025-05-28 00:43:32 +0100

    Add category button

--- src/UI/Menu.tsx
+++ src/UI/Menu.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
-import { DialogActions, DialogContent, TextField } from "@mui/material";
+import { Box, DialogActions, DialogContent, TextField } from "@mui/material";
 
 const Menu = () => {
   const [open, setOpen] = useState(false);
@@ -18,9 +18,14 @@ const Menu = () => {
   return (
     <>
       <div id="menu" className="panel">
-        <Button variant="contained" onClick={openExpense}>
-          Add
-        </Button>
+        <Box component="form" sx={{ display: "flex", flexDirection: "column" }}>
+          <Button variant="contained" onClick={openExpense} sx={{ mb: 2 }}>
+            Add Expense
+          </Button>
+          <Button variant="contained" onClick={openExpense}>
+            Add Category
+          </Button>
+        </Box>
       </div>
       <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth={true}>
         <DialogTitle>Add Expense</DialogTitle>

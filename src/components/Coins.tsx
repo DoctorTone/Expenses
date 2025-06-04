262fb1c182ccfa56da1cb97c8ebd30bd04251566
DoctorTone <tony@drt-software.com>
2025-06-04 01:29:40 +0100

    Add text labels to columns

--- src/components/Coins.tsx
+++ src/components/Coins.tsx
@@ -1,4 +1,4 @@
-import { Cylinder } from "@react-three/drei";
+import { Cylinder, Text } from "@react-three/drei";
 import useStore from "../state/store";
 import { useMemo } from "react";
 import { Vector3 } from "three";
@@ -6,34 +6,49 @@ import { Vector3 } from "three";
 const Coins = () => {
   const expenses = useStore((state) => state.expenses);
   const usedCategories = useStore((state) => state.usedCategories);
-  const dummyCats = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  const expenseTotals = useStore((state) => state.expenseTotals);
 
   const positions = useMemo(() => {
     // const startVector = new Vector3(0, 0, -3);
     const rotAxis = new Vector3(0, 1, 0);
     const interval = (2 * Math.PI) / 10;
-    let currentAngle = (dummyCats.length - 1) * (interval / 2);
-    const newPos = dummyCats.map((cat, index) => {
+    let currentAngle = (usedCategories - 1) * (interval / 2);
+    const positions = [];
+    for (let i = 0; i < usedCategories; ++i) {
       const startVector = new Vector3(0, 0, -20);
       startVector.applyAxisAngle(rotAxis, currentAngle);
       currentAngle -= interval;
-      return startVector;
-    });
-    return newPos;
+      positions.push(startVector);
+    }
+    return positions;
   }, [usedCategories]);
 
   // DEBUG
   console.log("Pos = ", positions);
   return (
     <>
-      {dummyCats.map((cat, index) => {
+      {Object.keys(expenseTotals).map((key, index) => {
         return (
-          <Cylinder
-            position={positions[index]}
-            args={[1, 1, expenses[0].amount * 2, 6]}
-          >
-            <meshStandardMaterial color="red" />
-          </Cylinder>
+          <>
+            <Cylinder
+              position={positions[index]}
+              args={[1, 1, expenseTotals[key] * 2, 6]}
+            >
+              <meshStandardMaterial color="red" />
+            </Cylinder>
+            <Text
+              color="black"
+              anchorX="center"
+              anchorY="middle"
+              position={[
+                positions[index].x,
+                expenseTotals[key] + 1,
+                positions[index].z,
+              ]}
+            >
+              {key}
+            </Text>
+          </>
         );
       })}
     </>
